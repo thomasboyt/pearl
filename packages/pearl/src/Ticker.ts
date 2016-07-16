bbe7041cba1dba6456230597921eaf409767aab6
Thomas Boyt <me@thomasboyt.com>
2016-07-16 01:54:32 -0400

    Fix ticker public api

--- packages/pearl/src/Ticker.ts
+++ packages/pearl/src/Ticker.ts
@@ -1,20 +1,22 @@
 export default class Ticker {
   private _nextTickFn: () => void;
+  private _loopFn: (dt: number) => void;
 
   time: number;
 
   constructor(gameLoop: (dt: number) => void) {
-    this.start(gameLoop);
+    this._loopFn = gameLoop;
+    this.start();
   }
 
-  start(gameLoop: (dt: number) => void) {
+  start() {
     this.time = Date.now();
 
     const tick = () => {
       const now = Date.now();
       const dt = now - this.time;
       this.time = now;
-      gameLoop(dt);
+      this._loopFn(dt);
       requestAnimationFrame(this._nextTickFn);
     };
 
