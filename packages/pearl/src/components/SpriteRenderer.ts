03dfb8311541bc28ce00edbf77998f65e44c6574
Thomas Boyt <me@thomasboyt.com>
2018-07-17 12:40:38 -0400

    SpriteRenderer: Add getCanvas() for Sledgehammer spawn/death animation

--- packages/pearl/src/components/SpriteRenderer.ts
+++ packages/pearl/src/components/SpriteRenderer.ts
@@ -14,7 +14,7 @@ export default class SpriteRenderer extends Component<Settings> {
   scaleX: number = 1;
   scaleY: number = 1;
 
-  create(settings: Settings) {
+  create(settings: Settings = {}) {
     if (settings.sprite) {
       this.sprite = settings.sprite;
     }
@@ -27,11 +27,6 @@ export default class SpriteRenderer extends Component<Settings> {
   }
 
   private _masked = false;
-
-  get masked() {
-    return this._masked;
-  }
-
   private _maskFrom: [number, number, number];
   private _maskTo: [number, number, number];
 
@@ -47,29 +42,48 @@ export default class SpriteRenderer extends Component<Settings> {
     delete this._maskTo;
   }
 
+  /**
+   * Returns the current sprite rendered to a canvas, with its top left corner at [0, 0], without
+   * any scaling or totation applied.
+   */
+  getCanvas(): HTMLCanvasElement {
+    const sprite = this.sprite!;
+    const canvas = document.createElement('canvas');
+    canvas.width = sprite.width;
+    canvas.height = sprite.height;
+    const ctx = canvas.getContext('2d')!;
+    this.draw(ctx, 0, 0);
+    return canvas;
+  }
+
+  /**
+   * Draw the sprite, without scale or rotation, to a passed canvas context
+   *
+   * This is intended to be used with a new external
+   */
+  private draw(ctx: CanvasRenderingContext2D, destX: number, destY: number) {
+    const sprite = this.sprite!;
+
+    if (this._masked) {
+      sprite.drawMasked(ctx, destX, destY, this._maskFrom, this._maskTo);
+    } else {
+      sprite.draw(ctx, destX, destY);
+    }
+  }
+
   render(ctx: CanvasRenderingContext2D) {
     if (!this.sprite) {
       return;
     }
 
     const phys = this.getComponent(Physical);
-
     ctx.translate(phys.center.x, phys.center.y);
-    ctx.rotate(phys.angle);
 
-    const sprite = this.sprite;
-
-    const destX = -sprite.width / 2;
-    const destY = -sprite.height / 2;
-
+    ctx.rotate(this.getComponent(Physical).angle);
     ctx.scale(this.scaleX, this.scaleY);
 
-    sprite.draw(ctx, destX, destY);
-
-    if (this.masked) {
-      sprite.drawMasked(ctx, destX, destY, this._maskFrom, this._maskTo);
-    } else {
-      sprite.draw(ctx, destX, destY);
-    }
+    const destX = -this.sprite.width / 2;
+    const destY = -this.sprite.height / 2;
+    this.draw(ctx, destX, destY);
   }
 }
