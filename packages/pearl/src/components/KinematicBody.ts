4496f258930e0f15785906f95dcf2dfe743d2b90
Thomas Boyt <me@thomasboyt.com>
2018-07-18 19:51:23 -0400

    Add KinematicBody component

---
+++ packages/pearl/src/components/KinematicBody.ts
@@ -0,0 +1,105 @@
+import Component from '../Component';
+import { CollisionInformation, ICollider, CollisionResponse } from './Collider';
+import Physical from './Physical';
+import { Coordinates } from '../types';
+import PolygonCollider from './PolygonCollider';
+
+export default class KinematicBody extends Component<null> {
+  moveAndCollide(vec: Coordinates): CollisionInformation[] {
+    const collisions = this._moveAndCollide(vec);
+    this.fireCollisions(collisions);
+    return collisions;
+  }
+
+  private _moveAndCollide(vec: Coordinates): CollisionInformation[] {
+    const phys = this.getComponent(Physical);
+
+    const prevCenter = { ...phys.center };
+    phys.translate(vec);
+
+    const collisions = this.getCollisions();
+
+    const solidCollisions = collisions.filter(
+      (collision) => !collision.collider.isTrigger
+    );
+
+    if (solidCollisions.length > 0) {
+      phys.center = prevCenter;
+    }
+
+    return collisions;
+  }
+
+  // TODO: This doesn't quite "slide" the way it probably should yet. A true
+  // slide would be that, if e.g. you were going northeast and were blocked by a
+  // wall on the west, you would then slide north with the _full magnitude of
+  // your velocity_, rather than _just the x component_ as happens here.
+  //
+  // Godot does this by saying "if there's a collision, use
+  // `velocity.slide(collision.normal)`", but I'm not sure how this works when
+  // it's possible to have multiple collisions at once.
+  //
+  // Notably, fixing this would solve some bugs around moving around corners, I
+  // think? Especially for AI.
+  moveAndSlide(vec: Coordinates): CollisionInformation[] {
+    const xCollisions = this._moveAndCollide({ x: vec.x, y: 0 });
+    const yCollisions = this._moveAndCollide({ x: 0, y: vec.y });
+    // remove duplicates
+    const collisions = [...new Set([...xCollisions, ...yCollisions])];
+    this.fireCollisions(collisions);
+    return collisions;
+  }
+
+  private fireCollisions(collisions: CollisionInformation[]) {
+    for (let collision of collisions) {
+      this.gameObject.onCollision(collision);
+      collision.collider.gameObject.onCollision(
+        new CollisionInformation(
+          this.gameObject.collider,
+          CollisionInformation.invertResponse(collision.response)
+        )
+      );
+    }
+  }
+
+  private getCollisions() {
+    const thisCollider = this.gameObject.collider;
+
+    if (!(thisCollider instanceof PolygonCollider)) {
+      throw new Error(
+        'Kinematic Body only supports PolygonCollider currently!'
+      );
+    }
+
+    const colliders = this.pearl.entities
+      .all()
+      .filter((entity) => entity !== this.gameObject)
+      .map((entity) => entity.collider)
+      .filter((collider) => collider) as ICollider[];
+
+    return colliders
+      .map((collider) => {
+        const isTrigger = collider.isTrigger;
+
+        // TODO: this "guard" is currently needed for some reason idk
+        let response: CollisionResponse | null;
+        if (collider instanceof PolygonCollider) {
+          response = collider.getCollision(thisCollider);
+        } else {
+          response = collider.getCollision(thisCollider);
+        }
+
+        if (!response) {
+          return null;
+        }
+
+        const collisionInformation = new CollisionInformation(
+          collider,
+          CollisionInformation.invertResponse(response)
+        );
+
+        return collisionInformation;
+      })
+      .filter((collision) => collision !== null) as CollisionInformation[];
+  }
+}
--- packages/pearl/src/index.ts
+++ packages/pearl/src/index.ts
@@ -19,7 +19,6 @@ export { default as Sprite } from './util/Sprite';
 export { default as SpriteSheet, ISpriteSheet } from './util/SpriteSheet';
 
 export { default as Physical } from './components/Physical';
-export { default as AnimationManager } from './components/AnimationManager';
 
 export {
   default as Collider,
@@ -28,7 +27,10 @@ export {
   CollisionInformation,
 } from './components/Collider';
 export { default as PolygonCollider } from './components/PolygonCollider';
-export { default as PolygonRenderer } from './components/PolygonRenderer';
 export { default as CircleCollider } from './components/CircleCollider';
+export { default as KinematicBody } from './components/KinematicBody';
+
+export { default as PolygonRenderer } from './components/PolygonRenderer';
 export { default as CircleRenderer } from './components/CircleRenderer';
 export { default as SpriteRenderer } from './components/SpriteRenderer';
+export { default as AnimationManager } from './components/AnimationManager';
