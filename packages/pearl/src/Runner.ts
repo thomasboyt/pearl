3f352b7a9b7816c98e14fc50f83953a1ce7733a3
Thomas Boyt <me@thomasboyt.com>
2018-08-02 15:48:46 -0400

    Update and document Runner class

--- packages/pearl/src/Runner.ts
+++ packages/pearl/src/Runner.ts
@@ -1,17 +1,25 @@
 export type RunFunction = () => void;
 
+/**
+ * This class can be used to manually add functions to run at the end of a run
+ * loop tick. It's primarily meant for internal use by dev tooling (like
+ * pearl-inspect), and may not remain public without a good use case.
+ */
 export default class Runner {
-  private _runs: RunFunction[] = [];
+  private _fns: RunFunction[] = [];
 
   update() {
-    for (let run of this._runs) {
-      run();
-    }
+    // iterate over a copy here so that functions added by runner functions
+    // (e.g. recursive loops) aren't run on this frame
+    const fns = [...this._fns];
+    this._fns = [];
 
-    this._runs = [];
+    for (let fn of fns) {
+      fn();
+    }
   }
 
   add(fn: RunFunction) {
-    this._runs.push(fn);
+    this._fns.push(fn);
   }
 }
