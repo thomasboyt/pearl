ec752a29d8db3786b5d83fd9fcbe8db6d4e0c799
Thomas Boyt <me@thomasboyt.com>
2018-08-04 08:36:26 -0400

    Fix es2015 imports not working in tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/packages'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(t|j)sx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   // workaround: https://github.com/facebook/jest/issues/6766
   testURL: 'http://localhost',
+  transformIgnorePatterns: ['node_modules/(?!pearl|@tboyt/coroutine-manager)'],
+  globals: {
+    'ts-jest': {
+      tsConfigFile: './jest.tsconfig.json',
+    },
+  },
 };
