5b77168572a343a487b8c3357755d1e82bd00726
Thomas Boyt <me@thomasboyt.com>
2018-08-08 19:56:23 -0400

    networking: Export NetworkingPlayer

--- packages/pearl-networking/src/index.ts
+++ packages/pearl-networking/src/index.ts
@@ -2,5 +2,8 @@ export { default as NetworkedEntity } from './components/NetworkedEntity';
 export { default as NetworkedPhysical } from './components/NetworkedPhysical';
 export { default as Networking } from './components/Networking';
 export { default as NetworkingClient } from './components/NetworkingClient';
-export { default as NetworkingHost } from './components/NetworkingHost';
+export {
+  default as NetworkingHost,
+  NetworkingPlayer,
+} from './components/NetworkingHost';
 export * from './types';
